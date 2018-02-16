9f341f88be5705296e30b7f9b4d0d4739627d512
Alex Owens <gamessecondwind@gmail.com>
2018-02-15 23:18:30 -0500

    Did part 3 of blog, adding basic Elm Architecture and setting up buttons to change the model.

--- src/Main.elm
+++ src/Main.elm
@@ -1,15 +1,55 @@
 module Main exposing (main)
 
 import Html exposing (..)
+import Html.Events exposing (onClick)
 
 main =
-    viewBlogPost
+    Html.beginnerProgram
+        { model = home
+        , update = update
+        , view = view
+        }
+
+-- Model
+
+type alias Model =
+    { title : String
+    , content: String
+    }
+
+home =
+    { title = "Elm Explorer Blog"
+    , content = "Here's what I learned while exploring Elm..."
+    }
+
+-- Add What I made with Elm Record
+whatIMadeWithElmPost =
+    { title = "What I Made With Elm"
+    , content ="Here's the blog post I made in Elm!"
+    }
+
+--Update
+
+type Msg
+    = ShowHomePage
+    | ShowWhatIMadeWithElmPost
 
 
-viewBlogPost =
-    body []
-        [ div []    
-            [ h1 [] [ text "Elm explorer blog"]
-            , text "Here's what I learned while exploring Elm..."
+update msg model =
+    case msg of
+        ShowHomePage ->
+            home
+        ShowWhatIMadeWithElmPost ->
+            whatIMadeWithElmPost
+
+--View
+
+view model =
+    body [] 
+        [ h1 [] [ text model.title]
+        , text model.content
+        , div []
+            [ button [ onClick ShowHomePage ] [ text "Home" ]
+            , button [ onClick ShowWhatIMadeWithElmPost ] [ text "What I Made With Elm" ]
             ]
         ]
